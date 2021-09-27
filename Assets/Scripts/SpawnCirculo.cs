f276f22cc21053e7805d3b1c1c7c7808bf87312b
Fabiano <Fabibinho2@gmail.com>
2021-09-27 01:44:30 -0300

    Comentando o código

--- Assets/Scripts/SpawnCirculo.cs
+++ Assets/Scripts/SpawnCirculo.cs
@@ -14,6 +14,7 @@ public class SpawnCirculo : MonoBehaviour{
     // Update is called once per frame
     void Update()
     {
+        //input da tecla enter, para dar spawn no circulo
         if(Input.GetKeyDown(KeyCode.Return)){
             Instantiate(prefabCirculo, new Vector3(0f, 0f, 0f), Quaternion.identity);
         }
